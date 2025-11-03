3e036b05b3f41ddc7fb9aacd562e0d254f0961f8
rahul12244044 <rahuljajoria2412@gmail.com>
2025-11-03 10:53:26 +0530

    all done

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const { aToken, setAToken, userData } = useContext(AppContext);
     const [showDropdownList, setShowDropdownList] = useState(false);
     const dropdownRef = useRef(null);
-    const adminPanelURL = "https://arhospitaladmin.netlify.app/";
+    const adminPanelURL = "https://arhospitaladminpanelar.netlify.app/";
 
     const logOut = () => {
         localStorage.removeItem("token");
